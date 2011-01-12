eaedb9d95ad94d2cf6dcc0a5caacc4904c8c3303
Dominik Eckelmann <eckelmann@cosmocode.de>
2011-01-12 17:34:13 +0100

    added german translation

---
+++ lang/de/lang.php
@@ -0,0 +1,28 @@
+<?php
+
+// settings must be present and set appropriately for the language
+$lang['encoding']   = 'utf-8';
+$lang['direction']  = 'ltr';
+
+// for admin plugins, the menu prompt to be displayed in the admin menu
+// if set here, the plugin doesn't need to override the getMenuText() method
+$lang['menu'] = 'Labeled Einstellungen';
+
+$lang['admin headline'] = "Labeled Plug-in";
+$lang['admin label name'] = "Label Name";
+$lang['admin label color'] = "Label Farbe";
+$lang['admin action'] = "Aktion";
+$lang['admin create'] = "Label erstellen";
+$lang['admin delete'] = "Label löschen";
+$lang['admin save'] = "Änderungen speichern";
+
+$lang['no input'] = 'Es konnte kein Label erstellt werden, da keine Daten eingegeben wurden.';
+$lang['no name'] = 'Sie müssen einen Labelnamen eingeben.';
+$lang['no color'] = 'Sie müssen eine Labelfarbe angeben.';
+$lang['label already exists'] = 'Es existiert bereits ein label mit diesem namen.';
+$lang['invalid color'] = 'Die angegebene Farbe ist ungültig. Bitte benutzen sie einen Farbcode wie z.B. #ff0000.';
+$lang['label deleted'] = 'Label gelöscht';
+
+$lang['label created'] = 'Label erstellt';
+
+
